6c2bae5d46a3ebf709abe4f8727bcbab7d31c97a
rhuairahrighairidh <ruaridh.odonnell@gmail.com>
2014-08-05 17:35:25 +0100

    make pip install dependencies

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 setup(
     name='SoN',
     version='0.1.0',
-    description='A visualisation library for nupic',
+    description='A visualisation library for nupic.',
     long_description='',
     url='https://github.com/rhuairahrighairidh/SoN',
     author="Ruaridh O'Donnell",
 
-    packages = ['SoN','SoN.plots'], #the final packages that will end up in site-packages
-    #py_modules = ['fileIO','plots'], #modules (python files) that will end up in site-packages
-    #package_dir = {'SoN':''},
+    packages = ['SoN','SoN.plots'], #packages (folders containing __init__.py) that will end up in site-packages
+    #py_modules = [#.py files#], #modules (python files) that will end up in site-packages
+    #package_dir = #a mapping from names of the packages to folders in the repo
 
-    #install_requires=['numpy','matplotlib', 'cerebro2'],
+    install_requires=['numpy','matplotlib'],
     
-    package_data = {'SoN': ['documentation/*.ipynb']}
+    package_data = {'SoN': ['documentation/*.ipynb']} #include the ipython notebook documentation
 
-    #data_files=[('docs', ['documentation/demo.ipynb'])] # don't use this, it puts the files in stupid places
+    #data_files=[('docs', ['documentation/demo.ipynb'])] # don't use this, it puts the files in silly places
 )
